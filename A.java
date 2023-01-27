3f4904813e115a2152e39b000c17bfde6df14fee
Mudit5149 <86524837+Mudit5149@users.noreply.github.com>
2023-01-27 11:57:17 +0530

    Add files via upload

---
+++ A.java
@@ -0,0 +1,17 @@
+
+//How to declare a constructor 
+
+package javaExample;
+
+public class A {
+	class Programming {
+		//constructor method
+		Programming() {
+		System.out.println("Constructor method called.");
+		}
+		public static void main(String[] args) {
+			A A = new A(); //creating object
+		}
+	}
+
+}
